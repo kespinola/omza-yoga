6255ba0ede2d2bcdf25ad9c33646581452513d2c
Kyle Espinola <ksespinola@gmail.com>
2014-01-06 16:39:30 -0800

    Changed survey questions

--- survey/survey-three.php
+++ survey/survey-three.php
@@ -3,7 +3,7 @@
 ?>
 
            	<div class="content">
-				<iframe src="http://edu.surveygizmo.com/s3/1499297/Omza-Class-Review-Three" style="width:100%; height: 1000px"></iframe>
+				<iframe src="http://edu.surveygizmo.com/s3/1499350/Omza-Class-Review-Three" style="width:100%; height: 1000px"></iframe>
 			</div>
 
  			</div>
--- survey/survey-two.php
+++ survey/survey-two.php
@@ -3,7 +3,7 @@
 ?>
 
            	<div class="content">
-				<iframe src="http://edu.surveygizmo.com/s3/1499294/Omza-Class-Review-Two" style="width:100%; height: 1000px"></iframe>
+				<iframe src="http://edu.surveygizmo.com/s3/1499348/Omza-Class-Review-Two" style="width:100%; height: 1000px"></iframe>
 			</div>
 
  			</div>

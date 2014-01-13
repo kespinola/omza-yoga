0c42b46946c06802101808afde38260cc8be9c85
Kyle Espinola <ksespinola@gmail.com>
2014-01-13 15:33:28 -0800

    Add about page

--- about.php
+++ about.php
@@ -5,9 +5,79 @@
 <?php
     include_once('nav-header.php');
 ?>
+<div class="container center-block about-page">
+	<div class="detail-wrap">
+		<div class="header-info">
+			<h1 class="info-header">About Us</h1>
+		</div>
+
+		<div class="row mission-section">
+
+			<div class="col-md-6 pull-left">
+				<div class="prof-overlay">
+					<img src="img/meditate-ocean.jpg" class="img-responsive pic-border" alt="Meditate ocean"></img>
+				</div>
+			</div>
+
+			<div class="col-md-6 pull-right">
+				<h2>Our Mission</h2>
+				<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+			</div>
+		</div>
+
+		<div class="value-section row">
+			<h2>Our Values</h2>
+			<hr>
+
+			<div class="col-md-4">
+				<div class="circle-background">
+					<i class="fa fa-heart"></i>
+				</div>
+				<h3>Passion</h3>
+				<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+			</div>
+
+			<div class="col-md-4">
+				<div class="circle-background">
+					<i class="fa fa-leaf"></i>
+				</div>
+				<h3>Wellness</h3>
+				<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+			</div>
+
+			<div class="col-md-4">
+				<div class="circle-background">
+					<i class="fa fa-user"></i>
+				</div>
+				<h3>You</h3>
+				<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+			</div>
+
+				<div class="our-team">
+			<h2>Our Team</h2>
+			<hr>
+			<div class="col-md-6">
+				<img src="img/kyle-espinola-pic.png" class="img-responsive img-circle founder-pic" alt="Kyle Omza founder picture"></img>
+				<h3>Kyle Espinola</h3>
+				<h4>Product & Co-Founder</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			</div>
+			<div class="col-md-6">
+				<img src="img/chyla-walsh-pic.png" class="img-responsive img-circle founder-pic" alt="Chyla Omza founder picture"></img>
+				<h3>Chyla Walsh</h3>
+				<h4>People & Co-Founder</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			</div>
+		</div>	
+	</div>
 
 
- </div> <!--close wrapper -->
+		</div>
+
+	</div>
+</div> <!-- Close Container Div -->
+
+</div> <!--close wrapper -->
  <?php include_once('script.php') ?>
     </body>
 </html>

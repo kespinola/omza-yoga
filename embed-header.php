7f8970c0926a97e03ad6b09aa4daabca72b85043
Kyle Espinola <ksespinola@gmail.com>
2014-01-17 10:04:33 -0800

    Edit embed branding
    add power by text styling

--- embed-header.php
+++ embed-header.php
@@ -1,3 +1,6 @@
   <div class="header">
+  	<div class="col-md-3 col-md-offset-5">
+       <span class="span-text"><em>powered by</em><span>
        <a href="#" class="logo t05">OMZA</a>
+   </div>
   </div>
